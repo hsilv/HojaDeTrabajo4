c323359ed954781b0775d28ffc50b8a2f5a459bb
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-03-06 13:58:07 -0600

    Update simCalculadora.java
    Menu

--- src/main/simCalculadora.java
+++ src/main/simCalculadora.java
@@ -6,7 +6,7 @@
 package main;
 
 import static main.InfixToPostfix.convInfixToPosfix;
-
+import java.util.Scanner;
 /**
  *
  * @author Sebastián
@@ -21,6 +21,27 @@ public class simCalculadora {
             CalculadoraPosfix calc = CalculadoraPosfix.getInstance();
             String cadena = convInfixToPosfix("5*(9+3)/6+5");
             System.out.println(cadena);
+            Scanner sc = new Scanner(System.in);
+            String menu= "1. ArrayList\n2. Vector\n3. Lista simple\n4. Lista doblemente encadenada";
+            System.out.println("¿Qué tipo de stack desa utilizar?\n"+menu);
+            int respuesta = sc.nextInt();
+            sc.next();
+            switch (respuesta) {
+                case 1:
+                    //trabajar con arraylist
+                    break;
+                case 2:
+                    //trabajar con vector
+                    break;
+                case 3:
+                    //trabajar con lista simple
+                    break;
+                case 4:
+                    //trabajar con lista doblemente encadenada
+                    break;
+                default:
+                    break;
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
