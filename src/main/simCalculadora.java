af3e9b263048d2b3ab9f9145e6b5da3605a67a0d
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-03-06 17:16:02 -0600

    Update simCalculadora.java
    Creación de menú de opciones

--- src/main/simCalculadora.java
+++ src/main/simCalculadora.java
@@ -7,6 +7,7 @@ package main;
 
 import static main.InfixToPostfix.convInfixToPosfix;
 import java.util.Scanner;
+import structures.*;
 /**
  *
  * @author Sebastián
@@ -19,26 +20,39 @@ public class simCalculadora {
     public static void main(String[] args) {
         try {
             CalculadoraPosfix calc = CalculadoraPosfix.getInstance();
+            
             String cadena = convInfixToPosfix("5*(9+3)/6+5");
             System.out.println(cadena);
             Scanner sc = new Scanner(System.in);
-            String menu= "1. ArrayList\n2. Vector\n3. Lista simple\n4. Lista doblemente encadenada";
+            String menu= "1. ArrayList\n2. Vector\n3. Lista ";
             System.out.println("¿Qué tipo de stack desa utilizar?\n"+menu);
             int respuesta = sc.nextInt();
-            sc.next();
             switch (respuesta) {
                 case 1:
                     //trabajar con arraylist
+                    System.out.println("El resultado de la expresion es: "+calc.Evaluar(cadena, 1));
                     break;
                 case 2:
                     //trabajar con vector
+                    System.out.println("El resultado de la expresion es: "+calc.Evaluar(cadena, 2));
                     break;
                 case 3:
                     //trabajar con lista simple
+                    System.out.println("¿Con qué tipo de lista desea trabajar?\n1. Simplemente Encadenada\n2. Doblemente encadenada");
+                    int opcion2 = sc.nextInt();
+                    switch(opcion2){
+                        case 1:
+                            System.out.println("El resultado de la expresion es: "+calc.Evaluar(cadena, 3));
+                            break;
+                        case 2:
+                            System.out.println("El resultado de la expresion es: "+calc.Evaluar(cadena, 4));
+                            break;
+                        default:
+                            System.out.println("Opción no válida");
+                        break;
+                    }
                     break;
-                case 4:
-                    //trabajar con lista doblemente encadenada
-                    break;
+                
                 default:
                     break;
             }
