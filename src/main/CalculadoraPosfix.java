2dd2fb6d5a9c8be855c342f6fee53bf3a910743c
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-03-06 17:15:46 -0600

    Update CalculadoraPosfix.java
    Implementacion de método Evaluar

--- src/main/CalculadoraPosfix.java
+++ src/main/CalculadoraPosfix.java
@@ -5,6 +5,9 @@
  */
 package main;
 
+import structures.IStack;
+import structures.Stack;
+
 /**
  *
  * @author Sebastián
@@ -22,5 +25,45 @@ public class CalculadoraPosfix {
             System.out.println("Ya se ha creado una calculadora antes");
         }
         return calc;
+    }
+    public int Evaluar(String expresion, int tipo){
+        IStack<Integer> lista= Stack.construir(tipo);
+        int resultado = 0;
+        int op1 = 0;
+        int op2 = 0;
+        String[] caracteres = expresion.split("");
+        
+        for (String caracter: caracteres){
+            if(caracter.equals("+") || caracter.equals("-") || caracter.equals("*") || caracter.equals("/")){
+                if(lista.count() <=1){
+                    throw new IllegalArgumentException("\nNo se puede realizar la operacion a falta de operadores.");
+                }else{
+                    op1 = lista.pull();
+                    op2 = lista.pull();
+                    switch(caracter){
+                        case "+":
+                            resultado = op1 + op2;
+                            break;
+                        case "-":
+                            resultado = op2 -op1;
+                            break;
+                        case "*":
+                            resultado = op1 * op2;
+                            break;
+                        case "/":
+                            if(op1 == 0){
+                                throw new IllegalArgumentException("\nNo se puede realizar la division entre 0.");
+                            }else{
+                                resultado = op2/op1;
+                            }
+                            break;
+                    }
+                    lista.push(resultado);
+                }
+            }else
+                lista.push(Integer.parseInt(caracter));
+        }
+        
+        return resultado;
     }
 }
