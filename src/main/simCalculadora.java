6cf45b2b87cf15957a6b0ea7e35019d3171fa3e5
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2022-03-06 22:10:47 -0600

    Update simCalculadora.java

--- src/main/simCalculadora.java
+++ src/main/simCalculadora.java
@@ -124,60 +124,6 @@ public class simCalculadora {
                 if(pregunta("\n¿Desea intentar de nuevo?\n1. Si.\n2. No.\nRepuesta:", 2) == 1) buclePrincipal = true;
                 else buclePrincipal = false;
             }
-
-
-            /*
-            try {
-                Scanner sc = new Scanner(System.in);
-                CalculadoraPosfix calc = CalculadoraPosfix.getInstance();
-                System.out.println("Ingrese la ruta donde se encuentre su archivo (datos.txt no incluye 'datos.txt' en la ruta)");
-                String ruta = sc.nextLine();
-                ruta = ruta +"\\datos.txt";
-                File archivo = new File(ruta);
-                ArrayList<String> lista = new ArrayList<String>();
-                if(!archivo.exists()){
-                    System.out.println("Verifique que el archivo exista, se encuentre en la misma carpeta y se llame 'datos.txt'");
-                }else{
-                    sw = new Scanner(archivo);
-                    while (sw.hasNextLine()){
-                        lista.add(sw.nextLine());
-                    }				    
-                }  
-                System.out.print(lista);
-                sw.close();
-                String menu= "1. ArrayList\n2. Vector\n3. Lista ";
-
-                
-                for(int i = 0; i<lista.size();i++){
-                    System.out.println("¿Qué tipo de stack desa utilizar para la línea "+(i+1)+"?\n"+menu);
-                    int respuesta = sc.nextInt();
-                    String cadena = convInfixToPosfix(lista.get(i));
-                    if(respuesta == 1){
-                        //trabajar con arraylist
-                        System.out.println("El resultado de "+ lista.get(i)+" es: "+calc.Evaluar(cadena, 1));
-                    }else if(respuesta == 2){
-                        //trabajar con vector
-                        System.out.println("El resultado de "+ lista.get(i)+" es: "+calc.Evaluar(cadena, 2));
-                    }else if (respuesta == 3){
-                        //trabaja con listas
-                        System.out.println("¿Con qué tipo de lista desea trabajar?\n1. Simplemente Encadenada\n2. Doblemente encadenada");
-                        int opcion2 = sc.nextInt();
-                        if(opcion2 == 1){
-                            //lista encadenada
-                            System.out.println("El resultado de "+ lista.get(i)+" es: "+calc.Evaluar(cadena, 3));
-                        }else if(opcion2 ==2){
-                            //lista doblemente encadenada
-                            System.out.println("El resultado de "+ lista.get(i)+" es: "+calc.Evaluar(cadena, 4));
-                        }else{
-                            System.out.println("Opción no válida");
-                        }
-                    }else{
-                        System.out.println("Opción no válida");
-                    }
-                }        
-            } catch (Exception e) {
-                System.out.println(e);
-            }*/
         }
 
     }
